c057b917057a39c1d85d33229783fbf9f7eb3d91
Kyle Phillips <kphillips375@yahoo.com>
2023-05-24 19:17:26 -0400

    added link for fonts

---
+++ src/root.tsx
@@ -0,0 +1,61 @@
+import { Suspense } from "solid-js";
+import {
+  Html,
+  Head,
+  Title,
+  Meta,
+  Link,
+  Body,
+  Routes,
+  FileRoutes,
+  Scripts,
+  ErrorBoundary,
+} from "solid-start";
+import Navbar from "./components/Navbar";
+import "./root.css";
+
+export default function Root() {
+
+  return (
+    <Html>
+      <Head>
+        <Title>Pieces</Title>
+        <Meta charset="utf-8" />
+        <Meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta name="description" content="Find your gem" />
+        <Link rel="manifest" href="/manifest.webmanifest" />
+        <Link
+          href="https://fonts.googleapis.com/css?family=Abril+Fatface"
+          rel="stylesheet"
+        />
+        {/* Had to add other google fonts in order to work */}
+        {/* Despite adding tailwind.config */}
+        <Link
+          href="https://fonts.googleapis.com/css?family=Raleway"
+          rel="stylesheet"
+        />
+         <Link
+          href="https://fonts.googleapis.com/css?family=Poppins"
+          rel="stylesheet"
+        />
+         <Link
+          href="https://fonts.googleapis.com/css?family=EB+Garamond"
+          rel="stylesheet"
+        />
+        <Link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet"/>
+      </Head>
+      <Body class="overflow-y-hidden">
+        <Suspense>
+          <ErrorBoundary>            
+            <Navbar />
+              <Routes>
+                <FileRoutes />
+              </Routes>
+          </ErrorBoundary>
+        </Suspense>
+        {/* This one line "scripts" voided my styling when deleted*/}
+        <Scripts />
+      </Body>
+    </Html>
+  );
+}
