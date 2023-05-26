0290d7419df66f5bd3a2e94258f73e395d767774
Kyle Phillips <kphillips375@yahoo.com>
2023-05-26 04:01:40 -0400

    about

---
+++ src/components/About.tsx
@@ -0,0 +1,16 @@
+import RibbonAnimation from "./motion/ribbon";
+
+const About = () => {
+  return (
+    <div>
+    <div class="text-white pt-44 pl-8 font-bold text-6xl md:text-10xl text-RichBlack font-raleway tracking-wider">
+      About
+      </div>
+      <div>
+        <RibbonAnimation />
+    </div>
+    </div>
+  );
+};
+
+export default About;
