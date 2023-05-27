5a355c93ff2bf4f9994ac32b4147fc71ece7ea7f
Kyle Phillips <kphillips375@yahoo.com>
2023-05-27 04:00:28 -0400

    some description

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -7,7 +7,10 @@ const About = () => {
       About
       </div>
       <div>
-        <RibbonAnimation />
+        <div class="text-white pt-14 pl-8 font-bold text-2xl md:text-4xl text-RichBlack font-raleway tracking-wider w-7/12">
+          AI generated leans and predictive tooling for highlighting increased matchup outcomes 
+        </div>
+        {/* <RibbonAnimation /> */}
     </div>
     </div>
   );
