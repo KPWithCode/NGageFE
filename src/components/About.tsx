67ece63975bd473ff7c42708b292bf6872bf2053
Kyle Phillips <kphillips375@yahoo.com>
2023-05-30 13:57:48 -0400

    about layout and remove unecessary imports

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,17 +1,21 @@
-import RibbonAnimation from "./motion/ribbon";
 
 const About = () => {
   return (
-    <div>
-    <div class="text-white pt-44 pl-8 font-bold text-6xl md:text-10xl text-RichBlack font-raleway tracking-wider">
-      About
+    <div class="">
+      <div class="text-white pt-32 pl-8 font-bold text-6xl md:text-10xl text-RichBlack font-raleway tracking-wider">
+        About
       </div>
       <div>
-        <div class="text-white pt-14 pl-8 font-bold text-2xl md:text-4xl text-RichBlack font-raleway tracking-wider w-7/12">
-          AI generated leans and predictive tooling for highlighting increased matchup outcomes 
+        <div class="text-white pt-8 pl-8 font-bold justify-right text-md md:text-lg text-RichBlack font-raleway tracking-wider w-4/12">
+          No Frills! No Guarantees! Pure mathematical approach to approaching sports
+          based outcomes.
         </div>
-        {/* <RibbonAnimation /> */}
-    </div>
+        <div class="text-white pt-14 pl-8 font-bold text-2xl md:text-4xl text-RichBlack font-raleway tracking-wider w-7/12">
+          AI generated leans and predictive tooling for highlighting increased
+          outcomes
+        </div>
+        
+      </div>
     </div>
   );
 };
