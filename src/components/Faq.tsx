572211329c10668cff838969bca8005fc1d9cd16
Kyle Phillips <kphillips375@yahoo.com>
2023-05-30 13:51:30 -0400

    faq setup

---
+++ src/components/Faq.tsx
@@ -0,0 +1,17 @@
+const FAQ = () => {
+    return (
+        <div>
+        <div class="flex text-white pt-32 pl-8 font-bold text-6xl md:text-10xl text-RichBlack font-raleway tracking-wider">
+            FAQ
+        </div>
+        <div class="text-white pt-8 pl-8 font-bold text-md md:text-3xl text-RichBlack font-raleway tracking-wider w-4/12">
+          Rule #1 of safe bet: <br /> <i>There no such thing as a safe bet. We don't
+          believe in one size fits all solutions. Sports betting is a market &
+          markets need tools.</i>
+        </div>
+        </div>
+
+    )
+}
+
+export default FAQ;
