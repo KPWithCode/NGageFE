283c2341b3b5d9b95ffe301ee9f623d7e4760039
Kyle Phillips <kphillips375@yahoo.com>
2023-05-30 19:46:14 -0400

    possible increase seo

--- src/root.tsx
+++ src/root.tsx
@@ -15,14 +15,17 @@ import Navbar from "./components/Navbar";
 import "./root.css";
 
 export default function Root() {
-
   return (
     <Html>
       <Head>
-        <Title>Pieces</Title>
+        <Title>StatPad - Sports Betting and Analytics</Title>
         <Meta charset="utf-8" />
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta name="description" content="Find your gem" />
+        <Meta
+          name="description"
+          content="Get accurate sports analytics, leans, and picks for sports bettors. Join StatPad for expert predictions and maximize your chances of winning."
+        />
+        <Meta name="keywords" content="sports analytics, leans, picks, sports bettors, sports predictions" />
         <Link rel="manifest" href="/manifest.webmanifest" />
         <Link
           href="https://fonts.googleapis.com/css?family=Abril+Fatface"
@@ -34,23 +37,26 @@ export default function Root() {
           href="https://fonts.googleapis.com/css?family=Raleway"
           rel="stylesheet"
         />
-         <Link
+        <Link
           href="https://fonts.googleapis.com/css?family=Poppins"
           rel="stylesheet"
         />
-         <Link
+        <Link
           href="https://fonts.googleapis.com/css?family=EB+Garamond"
           rel="stylesheet"
         />
-        <Link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet"/>
+        <Link
+          href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css"
+          rel="stylesheet"
+        />
       </Head>
       <Body>
         <Suspense>
-          <ErrorBoundary>            
+          <ErrorBoundary>
             <Navbar />
-              <Routes>
-                <FileRoutes />
-              </Routes>
+            <Routes>
+              <FileRoutes />
+            </Routes>
           </ErrorBoundary>
         </Suspense>
         {/* This one line "scripts" voided my styling when deleted*/}
