c4b14fb2a0c9d4fd90725a312d8566314cabeb4c
Kyle Phillips <kphillips375@yahoo.com>
2023-06-07 03:58:05 -0400

    subscription stripe integration. async dotenv

--- src/components/Subscribe/Subscription.tsx
+++ src/components/Subscribe/Subscription.tsx
@@ -1,8 +1,68 @@
 import { createSignal } from "solid-js";
 import SubscriptionCard from "./Card";
+import { loadStripe } from "@stripe/stripe-js";
+import dotenv from "dotenv";
 
+const init = async () => {
+  await dotenv.config();
+}
 const Subscription = () => {
+  init();
   const [toggle, setToggle] = createSignal();
+  const [isLoading, setLoading] = createSignal();
+  const [stripeError, setstripeError] = createSignal();
+
+  let stripePromise: any;
+  
+  const getStripe = () => {
+    if (!stripePromise) {
+      stripePromise = loadStripe(process.env.STRIPE_PUBLISHABLE_KEY || "");
+    }
+    return stripePromise;
+  }
+  const handleBasic = async () => {
+  //   setIsSignedIn(false);
+  setLoading(true)
+  const stripe = await getStripe();
+  const { error } = await stripe.redirectToCheckout({
+    lineItems: [
+      {
+        price: process.env.BASIC_STRIPE_PRICE_ID,
+        quantity: 1
+      }
+    ],
+    mode: "payment",
+    cancelUrl:window.location.origin,
+    success:`${window.location.origin}/purchased`
+  })
+
+  if (error) {
+    setLoading(false)
+    setstripeError(error)
+  }
+  };
+
+  const handlePremium = async () => {
+    //   setIsSignedIn(false);
+    setLoading(true)
+    const stripe = await getStripe();
+    const { error } = await stripe.redirectToCheckout({
+      lineItems: [
+        {
+          price: process.env.PREMIUM_STRIPE_PRICE_ID,
+          quantity: 1
+        }
+      ],
+      mode: "payment",
+      cancelUrl:window.location.origin,
+      success:`${window.location.origin}/purchased`
+    })
+  
+    if (error) {
+      setLoading(false)
+      setstripeError(error)
+    }
+    };
 
   return (
     <div class="">
@@ -12,23 +72,26 @@ const Subscription = () => {
       <div class="flex justify-center items-center h-auto w-screen mt-4">
         <div class="grid grid-cols-2 gap-16">
           <SubscriptionCard
+            onClick={handleBasic}
             title="Basic"
             description="Casual Bettors"
             features={[
-              "NBA, MLB, NHL, Big 5 Euro League ⚽",
-              "Limited Sports Optimiser",
+              "NBA, MLB, NHL, Big 5 Euro Leagues ⚽",
+              "Sports Optimiser",
               "Play of the day",
             ]}
-            price="14.99/mo"
+            price="19.99/mo"
           />
           <SubscriptionCard
+            onClick={handlePremium}
             title="Premium"
             description="Recreational Bettors & Sharps"
             features={[
-              "NHL, MLB, NHL, Big 5 Euro League ⚽",
+              "NHL, MLB, NHL, Big 5 Euro Leagues ⚽",
               "Sports Optimiser",
-              `VIP All access plays, Algo backed leans, Picks of the Day`,
-              
+              "VIP All access plays,",
+              "Algo Backed Leans",
+              "Picks of the Day"
             ]}
             price="29.99/mo"
             recommended="Recommended"
