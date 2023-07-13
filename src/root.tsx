ae7596d5152f6fbc1001ce673190d264d48a999a
Kyle Phillips <kphillips375@yahoo.com>
2023-07-13 19:52:06 -0400

    remove navbar from root

--- src/root.tsx
+++ src/root.tsx
@@ -60,7 +60,6 @@ export default function Root() {
       <Body>
         <Suspense>
           <ErrorBoundary>
-            <Navbar />
             <Routes>
               <FileRoutes />
             </Routes>
