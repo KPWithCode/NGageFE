4cd229f758c6b27fa48ec432b0291b230fee8dc8
Kyle Phillips <kphillips375@yahoo.com>
2023-07-14 03:46:33 -0400

    loginmodal showing..firebase wip

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -1,4 +1,4 @@
-import { createSignal } from "solid-js";
+import { createEffect, createSignal } from "solid-js";
 
 import {
   signInWithEmailAndPassword,
@@ -9,64 +9,64 @@ import {
   GoogleAuthProvider,
   signInWithPopup,
 } from "firebase/auth";
-import firebase from "../config/firebaseConfig";
+// import { app } from "../config/firebaseConfig";
 
-const LoginModal = async () => {
-  const [showModal, setShowModal] = createSignal(false);
+const LoginModal = ({ onClose }: { onClose: () => void }) => {
+  const [showModal, setShowModal] = createSignal(true);
   const [email, setEmail] = createSignal("");
   const [username, setUsername] = createSignal("");
   const [password, setPassword] = createSignal("");
   const [isSignup, setIsSignup] = createSignal(false);
+  
+  // const auth = getAuth(app);
+  // console.log({auth})
+  // const handleLoginWithEmail = async () => {
+  //   try {
+  //     await signInWithEmailAndPassword(auth, email(), password());
+  //     setShowModal(false);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
+  // const handleLoginWithGoogle = async () => {
+  //   const provider = new GoogleAuthProvider();
+  //   try {
+  //     await signInWithPopup(auth, provider);
+  //     setShowModal(false);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
 
-  const auth = getAuth(firebase);
+  // const handleSignupWithEmail = async () => {
+  //   try {
+  //     // Signup logic
+  //     await createUserWithEmailAndPassword(auth, email(), password());
+  //     setShowModal(false);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
 
-  const handleLoginWithEmail = async () => {
-    try {
-      await signInWithEmailAndPassword(auth, email(), password());
-      setShowModal(false);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  const handleLoginWithGoogle = async () => {
-    const provider = new GoogleAuthProvider();
-    try {
-      await signInWithPopup(auth, provider);
-      setShowModal(false);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  // const handleSignUpWithUsername = async () => {
+  //   try {
+  //     const credential = EmailAuthProvider.credential(username(), password());
+  //     await signInWithCredential(auth, credential);
+  //     setShowModal(false);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
 
-  const handleSignupWithEmail = async () => {
-    try {
-      // Signup logic
-      await createUserWithEmailAndPassword(auth, email(), password());
-      setShowModal(false);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const handleSignUpWithUsername = async () => {
-    try {
-      const credential = EmailAuthProvider.credential(username(), password());
-      await signInWithCredential(auth, credential);
-      setShowModal(false);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const handleLoginWithUsername = async () => {
-    try {
-      const credential = EmailAuthProvider.credential(username(), password());
-      await signInWithCredential(auth, credential);
-      setShowModal(false);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  // const handleLoginWithUsername = async () => {
+  //   try {
+  //     const credential = EmailAuthProvider.credential(username(), password());
+  //     await signInWithCredential(auth, credential);
+  //     setShowModal(false);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
 
   const handleShowModal = () => {
     setShowModal(true);
@@ -77,10 +77,10 @@ const LoginModal = async () => {
   };
 
   return (
-    <div>
+    <div class="fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center bg-black bg-opacity-50 h-full z-20">
       {showModal() && (
         <div>
-          <h2>{isSignup() ? "Sign Up" : "Login"}</h2>
+          <h2>{isSignup() == false ? "Login" : "SignUp"}</h2>
           <form>
             {isSignup() && (
               <>
@@ -98,9 +98,9 @@ const LoginModal = async () => {
                   value={password()}
                   onChange={(event) => setPassword(event.target.value)}
                 />
-                <button type="submit" onClick={handleSignupWithEmail}>
+                {/* <button type="submit" onClick={handleSignupWithEmail}>
                   Sign Up with Email
-                </button>
+                </button> */}
               </>
             )}
 
@@ -130,7 +130,7 @@ const LoginModal = async () => {
                   value={password()}
                   onChange={(event) => setPassword(event.target.value)}
                 />
-                {email() ? (
+                {/* {email() ? (
                   <button type="submit" onClick={handleLoginWithEmail}>
                     Login with Email
                   </button>
@@ -138,13 +138,13 @@ const LoginModal = async () => {
                   <button type="submit" onClick={handleLoginWithUsername}>
                     Login with Username
                   </button>
-                )}
+                )} */}
               </>
             )}
-            <button onClick={handleToggleSignUp}>
+            <button type="button" onClick={handleToggleSignUp}>
               {isSignup() ? "Switch to Login" : "Switch to Sign Up"}
             </button>
-            <button onClick={handleShowModal}>Close</button>
+            <button class="px-2" onClick={onClose}>Close</button>
           </form>
         </div>
       )}
